84415aa0f74656182237407c232bbf9183050de4
Swarnavo2003 <swarnavo2020@gmail.com>
2025-12-17 13:12:02 +0530

    feat: logging added to jwt security filter class and filter added to security config

--- auth-backend/src/main/java/in/swarnavo/auth_backend/config/SecurityConfig.java
+++ auth-backend/src/main/java/in/swarnavo/auth_backend/config/SecurityConfig.java
@@ -1,29 +1,54 @@
 package in.swarnavo.auth_backend.config;
 
+import in.swarnavo.auth_backend.security.JwtAuthenticationFilter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import tools.jackson.databind.ObjectMapper;
+
+import java.util.Map;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
+    @Autowired
+    private JwtAuthenticationFilter jwtAuthenticationFilter;
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf(AbstractHttpConfigurer::disable);
+        http.cors(Customizer.withDefaults());
+        http.sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
         http.authorizeHttpRequests(authorizeHttpRequest ->
                 authorizeHttpRequest
                         .requestMatchers(HttpMethod.POST, "/api/v1/auth/register").permitAll()
                         .requestMatchers("/api/v1/auth/login").permitAll()
                         .anyRequest().authenticated()
-        ).httpBasic(Customizer.withDefaults());
+        ).exceptionHandling(ex -> ex.authenticationEntryPoint((request, response, e) -> {
+            e.printStackTrace();
+            response.setStatus(401);
+            response.setContentType("application/json");
+            String message = "Unauthorized Access : " + e.getMessage();
+            Map<String, String> errorMap = Map.of(
+                    "message", message,
+                    "status", String.valueOf(HttpStatus.UNAUTHORIZED),
+                    "statusCode", String.valueOf(401)
+            );
+            var objectMapper = new ObjectMapper();
+            response.getWriter().write(objectMapper.writeValueAsString(errorMap));
+        })).addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
         return http.build();
     }
 
--- auth-backend/src/main/java/in/swarnavo/auth_backend/security/JwtAuthenticationFilter.java
+++ auth-backend/src/main/java/in/swarnavo/auth_backend/security/JwtAuthenticationFilter.java
@@ -8,6 +8,8 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
@@ -27,17 +29,21 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
     private final JwtService jwtService;
     private final UserRepository userRepository;
+    private Logger logger = LoggerFactory.getLogger(JwtAuthenticationFilter.class);
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         String header = request.getHeader("Authorization");
+        logger.info("Authorization header: {}", header);
+
         if(header != null && header.startsWith("Bearer ")) {
             String token = header.substring(7);
-            if(!jwtService.isAccessToken(token)) {
-                filterChain.doFilter(request, response);
-                return;
-            }
             try {
+                if(!jwtService.isAccessToken(token)) {
+                    filterChain.doFilter(request, response);
+                    return;
+                }
+
                 Jws<Claims> parse = jwtService.parse(token);
                 Claims payload = parse.getPayload();
                 String userId = payload.getSubject();
